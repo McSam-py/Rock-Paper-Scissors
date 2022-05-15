0b4397acaf4893a43ffec9630a3adc36cc9d1c79
McSam-py <ardisam9876@gmail.com>
2022-05-15 15:53:01 +0000

    comments added to code and readme file created

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Rock, paper, scissors game.
+#How the game is played.
+In this game the Computer makes a random choice between rock, paper and scissors.
+<br>The user is also given a chance to enter an input.
+<br>The user must enter either rock, paper or scissors depending on which is desirable for them.
+
+#How points are rewarded.
+Points and rewarded on these set of rules:
+1. Rock beats scissors: One point is rewarded to the whoever chooses rock. 
+2. Scissors cut paper: One point is rewarded to whoever chooses scissors.
+3. Paper covers rock: One point is rewarded to whoever chooses paper.
+4. No points are rewarded if t both players choose the same thing.
+
+
+
+
--- rockPaperScissors.java
+++ rockPaperScissors.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 
 public class rockPaperScissors {
     public static void main(String[] args) {
-        Scanner takeInput = new Scanner(System.in);
-        Random randomNum = new Random();
+        Scanner takeInput = new Scanner(System.in); //Creating an object of the Scanner class
+        Random randomNum = new Random(); //Creating an object of the Random class
         System.out.println("Welcome to rock, paper, scissors.");
         System.out.println("Enter your username: ");
-        String user2 = takeInput.nextLine();
-        String possibles[] = {"rock", "paper", "scissors"};
-        int computerScore = 0;
-        int user2Score = 0;
+        String user2 = takeInput.nextLine(); //Taking the user's name from the console
+        String possibles[] = {"rock", "paper", "scissors"}; //Array containing a list of options the computer can choose from.
+        int computerScore = 0; //Setting the initial score of the computer to Zero
+        int user2Score = 0; //Setting the initial score of the user to Zero
 
-        int count = 1;
+        int count = 1; //Variable to regulate the number of times a user can play the game.
         while (count <= 5) {
             //Obtaining the choice of the computer.
             int computerChoice = randomNum.nextInt(possibles.length);
@@ -22,10 +22,12 @@ public class rockPaperScissors {
             //Obtaining the user input or the choice of the user.
             System.out.println("Choose between rock, paper or scissors: ");
             String user2choice = takeInput.nextLine().toLowerCase();
+
+            //Conditions to check who will be awarded a point.
             if (user2choice.equals(possibles[computerChoice])) {
-                System.out.println("It's a tie!!!");
+                System.out.println("It's a tie!!!");//No points are awarded to any of the players.
             } else if (user2choice.equals("rock") && possibles[computerChoice].equals("paper")) {
-                computerScore++;
+                computerScore++;//a point is awarded to the computer
             } else if (user2choice.equals("paper") && possibles[computerChoice].equals("rock")) {
                 user2Score++;
             } else if (user2choice.equals("paper") && possibles[computerChoice].equals("scissors")) {
@@ -39,10 +41,16 @@ public class rockPaperScissors {
             } else {
                 System.out.println("You have entered an illegal input.");
             }
+
+            //Print statement to show which random choice the computer made after the user makes a choice.
             System.out.println("Computer Played: " + possibles[computerChoice]);
             count++;
         }
+
+        //Printing out the scores of the two users.
         System.out.println("\n-------Scores-------" + "\nComputer's Score: " + computerScore + "\n" + user2 + "'s score: " + user2Score);
+
+        //Condition to check who won at the end of the game.
         if (user2Score > computerScore) {
             System.out.println("\n" + user2 + " Won the game!!");
         } else if (user2Score == (computerScore)) {
